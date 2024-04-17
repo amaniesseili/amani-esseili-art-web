07cc2eeb242a8cb65c04a0aa73b82ae54cee40eb
amani esseili <esseiliamani@hotmail.com>
2024-04-17 11:55:13 +0200

    Installed Vue Router & created page components

---
+++ src/pages/Contact.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  
+
+}
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/partials/variables" as *;
+
+
+
+</style>
